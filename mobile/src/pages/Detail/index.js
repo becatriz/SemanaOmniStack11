0bdefa8139a9d60586a3e34720c4f7cb5fda67cf
Rebeca <becatriz7@gmail.com>
2020-03-27 02:37:47 -0400

    Mobile

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+import style from './styles'
+
+export default function Detail(){
+    return (
+        <View style={style.container}>
+            <View S
+            Z
+
+}
