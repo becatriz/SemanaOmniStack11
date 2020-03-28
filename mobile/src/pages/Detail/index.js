cb7288424dc1f3ce8eaeaa58afe6dc1bc7438721
Rebeca <becatriz7@gmail.com>
2020-03-28 02:28:43 -0400

    Mobile

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,13 +1,82 @@
+
 import React from 'react';
-import { View } from 'react-native';
-
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import logoImg from '../../assets/logo.png';
-import style from './styles'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer'
 
-export default function Detail(){
+import styles from './styles'
+
+export default function Detail() {
+
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const incident = route.params.incident;
+    const message = `Olá ${incident.name} estou entrando em contato, pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat
+        ('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`
+
+    function navigateBack() {
+        navigation.goBack();
+
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
+            body: message
+
+        })
+
+
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
+
+    }
+
     return (
-        <View style={style.container}>
-            <View S
-            Z
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <TouchableOpacity
+                    onPress={navigateBack}
+                >
+                    <Feather name="arrow-left" size={28} color="#E82041" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.incidents}>
+                <Text style={[styles.incidentsProperty, { margin: 0 }]}>ONG:</Text>
+                <Text style={styles.incidentsValue}>{incident.name} de {incident.city}/{incident.uf} </Text>
 
+                <Text style={styles.incidentsProperty}>CASO:</Text>
+                <Text style={styles.incidentsValue}>{incident.title}</Text>
+
+                <Text style={styles.incidentsProperty}>VALOR:</Text>
+                <Text style={styles.incidentsValue}>{incident.value}</Text>
+            </View>
+
+            <View style={styles.contactbox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <Text style={styles.actionText}>Email</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
+
+        </View>
+    );
 }
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator } from '@react-navigation/stack';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
@@ -10,11 +9,11 @@ import Detail from './pages/Detail';
 
 export default function Routes(){
     return(
-       <NavigationContainer>
-           <AppStack.Navigator screenOptions={{headerShown:false}}>
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{headerShown: false}}>
                 <AppStack.Screen name="Incidents" component={Incidents}/>
-               <AppStack.Screen name="Detail" component={Detail}/>
-           </AppStack.Navigator>
-       </NavigationContainer> 
+                <AppStack.Screen name="Detail" component={Detail}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
     );
 }
